f08aec65b4fce61876dc9cd00f872a9b204dd28e
FabioCostaa <fabio.glz4@gmail.com>
2023-05-25 15:52:12 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,119 @@
+const data = [
+  {
+    category: "Futebol",
+    questions: [
+      {
+        question: "Qual seleção venceu a Copa do Mundo de Futebol de 1998",
+        options: ["Alemanha", "França", "Brasil", "Argentina"],
+        answer: "França",
+        tip: "Disputou a final contra o Brasil",
+      },
+      {
+        question: "Qual jogador brasileiro é conhecido como O Fenômeno ?",
+        options: ["Pelé", "Ronaldinho Gaúcho", "Neymar", "Ronaldo"],
+        answer: "Ronaldo",
+        tip: "Teve um corte de cabelo icônico na copa de 2002",
+      },
+      {
+        question: "Qual jogador argentino é frequentemente comparado a Pelé ?",
+        options: ["Hernán Crespo", "Diego Maradona", "Gabriel Batistuta", "Sergio Agüero"],
+        answer: "Diego Maradona",
+        tip: "Ídolo no Napoli",
+      },
+      {
+        question: "Qual clube italiano é conhecido como A Velha Senhora ?",
+        options: ["Milan", "Inter de Milão", "Roma", "Juventus"],
+        answer: "Juventus",
+        tip: "Possui duas Champions League",
+      },
+      {
+        question: "Qual o único time brasileiro que possui 3 titulos mundiais",
+        options: ["Flamengo", "São Paulo", "Palmeiras", "Corinthians"],
+        answer: "São Paulo",
+        tip: "Conhecido por ser o maior time do Brasil",
+      },
+    ],
+  },
+  {
+    category: "Basquete",
+    questions: [
+      {
+        question: "Qual jogador é conhecido como The Black Mamba ?",
+        options: ["LeBron James", "Kevin Durant", "Kobe Bryant", "Stephen Curry"],
+        answer: "Kobe Bryant",
+        tip: "The Black Mamba foi um jogador icônico conhecido por sua dedicação e competitividade.",
+      },
+      {
+        question: "Qual é a franquia de basquete com o maior número de títulos da NBA?",
+        options: ["Los Angeles Lakers", "Boston Celtics", "Chicago Bulls", "Golden State Warriors"],
+        answer: "Boston Celtics",
+        tip: "A franquia com o maior número de títulos da NBA tem uma história rica e bem-sucedida.",
+      },
+      {
+        question: "Qual seleção nacional venceu o maior número de títulos na história dos Jogos Olímpicos de Basquete?",
+        options: ["Espanha", "Argentina", "Estados Unidos", "Rússia"],
+        answer: "Estados Unidos",
+        tip: "A seleção nacional mais vitoriosa nos Jogos Olímpicos de Basquete é uma potência no esporte.",
+      },
+      {
+        question: "Qual jogador é o recordista de triplos-duplos na NBA?",
+        options: [
+          "Russell Westbrook",
+          "LeBron James",
+          "Magic Johnson",
+          "Larry Bird"],
+        answer: "Russell Westbrook",
+        tip: "O recordista de triplos-duplos na NBA é um jogador de grande versatilidade.",
+      },
+      {
+        question: "Qual jogador é o recordista de pontos marcados na história da NBA?",
+        options: ["LeBron James", "Kobe Bryant", "Karl Malone", "Kareem Abdul-Jabbar"],
+        answer: "Kareem Abdul-Jabbar",
+        tip: "Atualmente está com 76 anos",
+      },
+    ],
+  },
+  {
+    category: "Volei",
+    questions: [
+      {
+        question: "Quantos jogadores compõem uma equipe de vôlei em quadra?",
+        options: [
+          "5",
+          "7",
+          "8",
+          "6",
+        ],
+        answer: "6",
+        tip:"Número par",
+      },
+      {
+        question: "Qual é o termo usado para descrever o toque realizado com as pontas dos dedos das mãos?",
+        options: ["Manchete", "Saque", "Levantamento", "Toque"],
+        answer: "Levantamento",
+        tip:"Usado para passar a bola para um atacante",
+      },
+      {
+        question: "Quantos pontos são necessários para vencer um set de vôlei?",
+        options: ["19 pontos", "20 pontos", "22 pontos", "25 pontos"],
+        answer: "25 pontos",
+        tip:"Número impar",
+      },
+      {
+        question: "Qual é o nome dado ao golpe no qual o jogador ataca a bola com força para marcar um ponto?",
+        options: ["Saque", "Bloqueio", "Manchete", "Cortada"],
+        answer: "Cortada",
+        tip:"É um ataque potente em que o jogador golpeia a bola com força para superar o bloqueio adversário.",
+      },
+      {
+        question:
+          "Qual jogador é responsável por iniciar o jogo com o saque?",
+        options: ["O oposto", "O líbero", "O atacante", "O levantador"],
+        answer: "O levantador",
+        tip:"Responsável por iniciar o jogo com o saque, lançando a bola para o campo adversário.",
+      },
+    ],
+  },
+];
+
+export default data;
